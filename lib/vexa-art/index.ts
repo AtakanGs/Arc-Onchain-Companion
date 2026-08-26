ce238504203a313ac7aef77b4ec83fd1306ea8e5
Atakan <97318753+AtakanGs@users.noreply.github.com>
2026-08-26 22:57:50 +0300

    design: expose locked Vexa genesis artwork

---
+++ lib/vexa-art/index.ts
@@ -0,0 +1,8 @@
+import hero0 from "./hero-0";
+import hero1 from "./hero-1";
+import hero2 from "./hero-2";
+import hero3 from "./hero-3";
+import hero4 from "./hero-4";
+import hero5 from "./hero-5";
+
+export const vexaGenesisArt = `data:image/webp;base64,${hero0}${hero1}${hero2}${hero3}${hero4}${hero5}`;
