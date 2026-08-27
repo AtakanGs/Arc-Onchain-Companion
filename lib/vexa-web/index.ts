ee65c584657e605835a758174560f3732279d4b3
Atakan <97318753+AtakanGs@users.noreply.github.com>
2026-08-27 09:21:45 +0300

    fix: assemble verified Vexa web assets

---
+++ lib/vexa-web/index.ts
@@ -0,0 +1,12 @@
+import vexa0 from "./vexa-0";
+import vexa1 from "./vexa-1";
+import vexa2 from "./vexa-2";
+import vexa3 from "./vexa-3";
+import veyra0 from "./veyra-0";
+import veyra1 from "./veyra-1";
+import vexus0 from "./vexus-0";
+import vexus1 from "./vexus-1";
+
+export const vexaGenesisArt = `data:image/webp;base64,${vexa0}${vexa1}${vexa2}${vexa3}`;
+export const veyraPreviewArt = `data:image/webp;base64,${veyra0}${veyra1}`;
+export const vexusPreviewArt = `data:image/webp;base64,${vexus0}${vexus1}`;
