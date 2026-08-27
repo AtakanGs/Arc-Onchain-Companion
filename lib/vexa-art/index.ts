635af27baca504fc7436b9720d00c121426e7a9f
Atakan <97318753+AtakanGs@users.noreply.github.com>
2026-08-27 09:25:30 +0300

    fix: route Vexa Genesis through verified assets

--- lib/vexa-art/index.ts
+++ lib/vexa-art/index.ts
@@ -1,8 +1,1 @@
-import hero0 from "./hero-0";
-import hero1 from "./hero-1";
-import hero2 from "./hero-2";
-import hero3 from "./hero-3";
-import hero4 from "./hero-4";
-import hero5 from "./hero-5";
-
-export const vexaGenesisArt = `data:image/webp;base64,${hero0}${hero1}${hero2}${hero3}${hero4}${hero5}`;
+export { vexaGenesisArt } from "../vexa-web";
