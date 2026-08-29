43746f67fe4b3f753eb75d57a0048dc95de4c4f0
Atakan <97318753+AtakanGs@users.noreply.github.com>
2026-08-29 16:11:42 +0300

    fix: assemble exact Vexa source asset

---
+++ lib/vexa-source/index.ts
@@ -0,0 +1,27 @@
+import part00 from "./part-00";
+import part01 from "./part-01";
+import part02 from "./part-02";
+import part03 from "./part-03";
+import part04 from "./part-04";
+import part05 from "./part-05";
+import part06 from "./part-06";
+import part07 from "./part-07";
+import part08 from "./part-08";
+import part09 from "./part-09";
+import part10 from "./part-10";
+
+export const vexaGenesisBase64 = [
+  part00,
+  part01,
+  part02,
+  part03,
+  part04,
+  part05,
+  part06,
+  part07,
+  part08,
+  part09,
+  part10,
+].join("");
+
+export const vexaGenesisArt = `data:image/webp;base64,${vexaGenesisBase64}`;
